8e6b6e5d2d4af112b5bf7618513e79e07e9db176
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-01-16 10:39:58 +0100

    build fix

--- src/main/scala/printable/workshop/PrintableSyntaxWorkshop.scala
+++ src/main/scala/printable/workshop/PrintableSyntaxWorkshop.scala
@@ -5,7 +5,7 @@ object PrintableSyntaxWorkshop {
   implicit class PrintableOps[A](value: A) {
     def format(implicit printable: PrintableWorkshop[A]): String = null // use PrintableWorkshop.format
 
-    def print(implicit printable: PrintableWorkshop[A]): Unit = // use PrintableWorkshop.print
+    def print(implicit printable: PrintableWorkshop[A]): Unit = null // use PrintableWorkshop.print
   }
 
 }
